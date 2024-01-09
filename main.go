4d1975dadd42e543dcd32c24f4badb59378b9c1c
ZShab Niba <44338441+nibazshab@users.noreply.github.com>
2024-01-09 14:48:53 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+    "fmt"
+    "net/http"
 )
 
 func main() {
-  http.HandleFunc("/0gxFztNrJBeBsCcwuTBdQbxchPDcVEyL", handleWebhook)
-  http.ListenAndServe(":10001", nil)
+    http.HandleFunc("/0gxFztNrJBeBsCcwuTBdQbxchPDcVEyL", handleWebhook)
+    http.ListenAndServe(":10001", nil)
 }
 
 func handleWebhook(w http.ResponseWriter, r *http.Request) {
-  if r.Method == "POST" {
-    fmt.Fprint(w, "OK")
-  }
+    if r.Method == "POST" {
+        fmt.Fprint(w, "OK")
+    }
 }
